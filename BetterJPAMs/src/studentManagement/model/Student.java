52040ba38ec1a95cb9751d860fd33f01856b6d90
ceb-student <CEB-Student@caddoschools.org>
2017-08-28 09:20:05 -0500

    Made a student class

---
+++ BetterJPAMs/src/studentManagement/model/Student.java
@@ -0,0 +1,30 @@
+package studentManagement.model;
+
+public class Student 
+{
+	boolean isCurrentStudent;
+	int gradeLevel;
+	int idNumber;
+	String name;
+	String gender;
+	int age;
+	double gpa; 
+	
+	public Student()
+	{
+		isCurrentStudent = false;
+		gradeLevel = 0;
+		idNumber = 000000;
+		name = "";
+		gender = "";
+		age = 0;
+		gpa = 0.0;
+	}
+	
+	public String toString()
+	{
+		String str = "";
+		return str;
+	}
+	
+}
