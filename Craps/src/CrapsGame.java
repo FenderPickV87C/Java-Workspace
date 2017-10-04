25b8e5171b5799977c9b5b92d37f8c647ef75225
ceb-student <CEB-Student@caddoschools.org>
2017-10-04 09:17:06 -0500

    Tried to fix the code

--- Craps/src/CrapsGame.java
+++ Craps/src/CrapsGame.java
@@ -14,9 +14,20 @@ public class CrapsGame
    */
   public int processRoll(int total)
   {
-
-    __________________________________________
-    ...
+	  int result = 0;
+	  if (total == 7 || total == 11)
+	  {
+		  result = 1;
+	  }
+	  else if (total == 2 || total == 3 || total == 12)
+	  {
+		  result = -1;
+	  }
+	  else
+	  {
+		  result = 0;
+		  point = total;
+	  }
 
     return result;
   }
